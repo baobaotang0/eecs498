756ea1936dfb5b4fb47ac426ba986664b48c9f3a
baobaotang <baobaotang0@gmail.com>
2022-12-11 02:52:37 -0500

    ME599 pset6 other's ans from github short version

--- pset56/test.py
+++ pset56/test.py
@@ -1,75 +1,38 @@
-import os,sys,inspect
-current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parent_dir = os.path.dirname(current_dir)
-sys.path.insert(0, parent_dir)
-
 import numpy as np
 import matplotlib.pyplot as plt
 
-from oed import OED
-from oed.utils import *
 
-def model_1(theta, d):
-    """
-    Nonliner model.
+def norm_logpdf(x, loc=0.0, scale=1.0):
+    return -np.log(np.sqrt(2 * np.pi) * scale) - (x - loc) ** 2 / 2 / scale ** 2
 
-    Parameters
-    ----------
-    stage : int
-        The stage index of the experiment.
-    theta : np.ndarray of size (n_sample or 1, n_param)
-        The value of unknown linear model parameters.
-    d : np.ndarray of size (n_sample or 1, n_design)
-        The design variable.
 
-    Returns
-    -------
-    numpy.ndarray of size (n_sample, n_obs)
-        The output of the linear model.
-    """
+def norm_pdf(x, loc=0.0, scale=1.0):
+    return np.exp(-(x - loc) ** 2 / 2 / scale ** 2) / scale / np.sqrt(2 * np.pi)
+
+
+def G(theta, d):
     return theta ** 3 * d ** 2 + theta * np.exp(-np.abs(0.2 - d))
 
-n_param = 1 # Number of parameters.
-n_design = 1 # Number of design variables.
-n_obs = 1 # Number of observations.
 
-low = 0
-high = 1
-prior_rvs = lambda n_sample: np.random.uniform(low=low,
-                                               high=high,
-                                               size=(n_sample, n_param))
-prior_logpdf = lambda theta: uniform_logpdf(theta,
-                                            low=low,
-                                            high=high)
+def utility(d, thetas, noises, sigma):
+    n_sample = len(thetas)
+    Gs = G(thetas, d)
+    # print("Gs.shape",Gs.shape)
+    ys = Gs + sigma * noises
+    # print("ys", ys.shape, self.noise_loc, sigma , self.noise_r_s)
+    loglikelis = norm_logpdf(ys, Gs, sigma)
+    evids = np.array([np.mean(norm_pdf(ys[i:i + 1], Gs, sigma)) for i in range(n_sample)])
+    return np.mean(loglikelis - np.log(evids))
 
-design_bounds = [(0, 1),] # lower and upper bounds of design variables.
 
-# Noise if following N(noise_loc, noise_base_scale + noise_ratio_scale * abs(G))
-noise_loc = 0
 noise_base_scale = 0.01
-noise_ratio_scale = 0
-noise_info = [(noise_loc, noise_base_scale, noise_ratio_scale),]
 
-# Random state could be eith an integer or None.
-random_state = 2021
-
-oed_1 = OED(model_fun=model_1,
-            n_param=n_param,
-            n_design=n_design,
-            n_obs=n_obs,
-            prior_rvs=prior_rvs,
-            design_bounds=design_bounds,
-            noise_info=noise_info,
-            prior_logpdf=prior_logpdf,
-            reward_fun=None,
-            random_state=random_state)
-
-ds = np.linspace(design_bounds[0][0], design_bounds[0][1], 21)
+ds = np.linspace(0, 1, 21)
 Us = []
-thetas = prior_rvs(1000)
-noises = np.random.normal(size=(1000, n_obs))
+thetas = np.random.uniform(size=(1000, 1))
+noises = np.random.normal(size=(1000, 1))
 for d in ds:
-    Us.append(oed_1.exp_utility(d, thetas, noises))
+    Us.append(utility(d, thetas, noises, noise_base_scale))
 
 plt.figure(figsize=(6, 4))
 plt.plot(ds, Us)
